d09f8d3e4d3a7392c72ba0b20c1ffd4eae9aee1b
Kofi Ackah <kofiackah360@gmail.com>
2024-08-30 19:06:23 +0000

    Created Custom Button

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -14,6 +14,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
       />
       <Text
         className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -24,7 +25,19 @@ const TabIcon = ({ icon, color, name, focused }) => {
 const TabLayout = () => {
   return (
     <>
-      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            height: 65,
+            backgroundColor: "#161622",
+            borderTopColor: "#232533",
+            borderTopWidth: 1,
+          },
+        }}
+      >
         <Tabs.Screen
           name="Home"
           options={{
@@ -41,21 +54,6 @@ const TabLayout = () => {
           }}
         />
         <Tabs.Screen
-          name="Create"
-          options={{
-            title: "Create",
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon={icons.create}
-                name="Create"
-                color={color}
-                focused={focused}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
           name="Bookmark"
           options={{
             title: "Bookmark",
@@ -64,6 +62,21 @@ const TabLayout = () => {
               <TabIcon
                 icon={icons.bookmark}
                 name="Bookmark"
+                color={color}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                name="Create"
                 color={color}
                 focused={focused}
               />
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
+import { View, Text, ScrollView, Image } from "react-native";
+import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/Home">Go to home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center min-h-[85vh] items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View>
+            <Text className="text-3xl text-white text-center font-bold mx-4">
+              Discover Endless Possibiliteis with{"  "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-6"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exloration with Aora
+          </Text>
+          <CustomButton
+            containerStyles="mt-5 w-full"
+            title="Continue with Email"
+            handlePress={() => router.push("/SignIn")}
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
